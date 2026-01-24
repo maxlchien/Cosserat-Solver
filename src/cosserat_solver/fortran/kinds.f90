59d3c3ada46db198845235d1db8d107d439d9cd6
maxlchien <90492587+maxlchien@users.noreply.github.com>
2026-01-24 17:41:56 -0500

    Set up basic meson structure

---
+++ src/cosserat_solver/fortran/kinds.f90
@@ -0,0 +1,8 @@
+module cosserat_kinds
+  use iso_fortran_env, only: real128, int32
+  implicit none
+
+  integer, parameter :: rk = real128
+  integer, parameter :: ik = int32
+
+end module cosserat_kinds
