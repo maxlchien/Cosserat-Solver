b0fa0eeefca00b23d77a2932d61cdb7e42334397
maxlchien <90492587+maxlchien@users.noreply.github.com>
2026-07-04 18:46:01 -0400

    Fix simulations list that got broken when _3d was removed

--- tests/displacement-tests/conftest.py
+++ tests/displacement-tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 # read the data folder and get all the subfolders which have complete reference and param data
 
-SIMULATIONS = ["cosserat_benchmark", "elastic_benchmark"]
+SIMULATIONS = ["cosserat_3d_benchmark", "elastic_3d_benchmark"]
 
 
 @pytest.fixture(params=SIMULATIONS, scope="session")
